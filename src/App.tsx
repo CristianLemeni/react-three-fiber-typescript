d9ed39196cb03f4c2fff916875d9a58609532d7f
cristianlemeni <37481559+CristianLemeni@users.noreply.github.com>
2021-10-19 18:41:18 +0300

    collision
    star movement and collision with box, so that they don't exist that space

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Canvas } from 'react-three-fiber';
 import Controls from './components/Controls';
 import Scene from './components/Scene';
 import './App.css';
-import { RoundedBox } from "@react-three/drei";
+
+// function Plane({ position }: any) {
+
 
 
 export default function App (){
+  const [showPlane, set] = useState(true)
   return(
     <Canvas>
     <Scene/>
     <Controls/>
-    <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4} onPointerEnter={undefined} onPointerLeave={undefined} onPointerMissed={undefined} onPointerCancel={undefined}></RoundedBox>
   </Canvas>
   )
 }
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,37 +1,86 @@
-import React, { useRef } from "react";
-import { useFrame } from "react-three-fiber";
-import { Mesh, Vector3 } from "three";
-import { Stats, PerspectiveCamera } from "@react-three/drei";
+import React, { useEffect, useRef, useState } from "react";
+// import { useFrame } from "react-three-fiber";
+import { Mesh, Vector3, Matrix4, Box3, MeshBasicMaterial, Color } from "three";
+import { Stats } from "@react-three/drei";
+import { gsap } from "gsap";
+
+const allStars: Array<React.MutableRefObject<undefined>> = []
 
 const Star = ({ position, color }: { position: any, color: any }) => {
-  const mesh = useRef(null);
-  useFrame(() => (
-    (mesh.current! as Mesh).rotation.x += 0.01)
-  );
+  const mesh = useRef();
+  const [hover, setHover] = useState(false);
+  const radius = parseFloat((Math.random() * (0.5 - 0.2) + 0.2).toFixed(4))
+  allStars.push(mesh)
+  useEffect(() => {
+    moveStar(mesh, Math.floor(Math.random() * 20) - 10, Math.floor(Math.random() * 20) - 10, Math.floor(Math.random() * 20) - 10, radius).play()
+  });
   return (
     <>
-      <mesh position={position} ref={mesh}>
-        <sphereBufferGeometry attach='geometry' args={[0.02, 15, 16]} />
-        <meshStandardMaterial attach='material' color={color} />
+      <mesh position={position} ref={mesh}
+        onPointerOver={() => {
+          setHover(true);
+        }}
+        onPointerOut={() => {
+          setHover(false);
+        }}
+      >
+        <sphereBufferGeometry attach='geometry' args={[radius, 15, 16]} />
+        <meshStandardMaterial attach='material' color={hover ? "red" : color} />
       </mesh>
 
     </>
   );
 }
 
+function moveStar(mesh: React.MutableRefObject<undefined>, x: number, y: number, z: number, r: number) {
+  const t = gsap.to((mesh.current! as Mesh).position, {
+    x: x,
+    y: y,
+    z: z,
+    duration: 10,
+    onUpdate: () => {
+      if (isInsideBox(mesh, r)) {
+        t.kill()
+        moveStar(mesh, Math.floor(Math.random() * 20) - 10, Math.floor(Math.random() * 20) - 10, Math.floor(Math.random() * 20) - 10, r).play()
+      }
+    },
+    onComplete: () => {
+      moveStar(mesh, Math.floor(Math.random() * 20) - 10, Math.floor(Math.random() * 20) - 10, Math.floor(Math.random() * 20) - 10, r).play()
+    }
+  });
+  return t
+}
+
+function isInsideBox(self: React.MutableRefObject<undefined>, r: number): boolean {
+  if (((self.current! as Mesh).position.x + (r*2)) > 5 || ((self.current! as Mesh).position.x - r) < -5) {
+    return true
+  }
+  else if (((self.current! as Mesh).position.y + r) > 5 || ((self.current! as Mesh).position.y - r) < -5) {
+    return true
+  }
+  else if (((self.current! as Mesh).position.z + r) > 5 || ((self.current! as Mesh).position.z - r) < -5) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
 function createStars() {
   let stars = []
-  for (var i = 0; i < 100; i++) {
-    var x = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5)
-    var y = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5)
-    var z = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5)
+  for (let i = 0; i < 100; i++) {
+    const x = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5)
+    const y = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5)
+    const z = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5)
     stars[i] = [x, y, z]
   }
   return stars
 }
 
 const stars = createStars().map((cords, i) =>
-  (<Star key={i} position={cords} color='white' />)
+(
+  <Star key={i} position={cords} color='white' />
+)
 )
 
 function Box({ position }: { position: Vector3 }) {
