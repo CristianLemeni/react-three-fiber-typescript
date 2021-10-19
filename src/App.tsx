3b13a6033001d9c8463d62a9235fefa9025d9ad9
cristianlemeni <37481559+CristianLemeni@users.noreply.github.com>
2021-10-19 11:32:09 +0300

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { Canvas } from 'react-three-fiber';
 import Controls from './components/Controls';
 import Scene from './components/Scene';
 import './App.css';
+import { RoundedBox } from "@react-three/drei";
 
-const App: React.FC = () => (
-  <Canvas>
-    <Scene />
-    <Controls />
+
+export default function App (){
+  return(
+    <Canvas>
+    <Scene/>
+    <Controls/>
+    <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4} onPointerEnter={undefined} onPointerLeave={undefined} onPointerMissed={undefined} onPointerCancel={undefined}></RoundedBox>
   </Canvas>
-);
-
-export default App;
+  )
+}
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { useFrame } from "react-three-fiber";
+import { Mesh, Vector3 } from "three";
+import { Stats, PerspectiveCamera } from "@react-three/drei";
 
-const Scene: React.FC = () => (
+const Star = ({ position, color }: { position: any, color: any }) => {
+  const mesh = useRef(null);
+  useFrame(() => (
+    (mesh.current! as Mesh).rotation.x += 0.01)
+  );
+  return (
+    <>
+      <mesh position={position} ref={mesh}>
+        <sphereBufferGeometry attach='geometry' args={[0.02, 15, 16]} />
+        <meshStandardMaterial attach='material' color={color} />
+      </mesh>
+
+    </>
+  );
+}
+
+function createStars() {
+  let stars = []
+  for (var i = 0; i < 100; i++) {
+    var x = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5)
+    var y = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5)
+    var z = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5)
+    stars[i] = [x, y, z]
+  }
+  return stars
+}
+
+const stars = createStars().map((cords, i) =>
+  (<Star key={i} position={cords} color='white' />)
+)
+
+function Box({ position }: { position: Vector3 }) {
+  return (
+    <mesh castShadow position={position}>
+      <boxBufferGeometry attach="geometry" args={[10, 10, 10]} />
+      <meshStandardMaterial attach="material" color="0xffffff" wireframe={true} />
+    </mesh>
+  );
+}
+
+
+const Scene: React.FC = (props) => (
   <mesh>
-    <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-    <meshNormalMaterial attach="material" />
+    <Box position={new Vector3(0, 0, 0)} />
+    <ambientLight />
+    <Stats showPanel={0} className="stats" {...props} />
+    {stars}
   </mesh>
 );
 
 export default Scene;
+
+
